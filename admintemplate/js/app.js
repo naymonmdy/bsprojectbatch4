cde05cf43906ebede287f971f49f9a09faa06f8f
naymonmdy <naymon2021@gmail.com>
2024-05-27 00:45:29 +0630

    carousel slide and data-bs-ride

--- admintemplate/js/app.js
+++ admintemplate/js/app.js
@@ -1,3 +1,22 @@
-$(document).ready(function () {
- 
-});
+
+    // Start Navbar
+        // start top bar
+            function dropbtn(e){
+                // console.log(e.target);
+                // console.log(e.target.parentElement.nextElementSibling);
+                e.target.parentElement.nextElementSibling.classList.toggle("show");
+            };
+        // end top bar
+    // End Navbar
+
+$(document).ready(function(){
+     // Start Navbar
+            // Start left side bar
+            $(".sidebarlinks").click(function(){
+                $(".sidebarlinks").removeClass("currents");
+                $(this).addClass("currents");
+            })
+            // End left side bar
+     // End Navbar
+
+})
